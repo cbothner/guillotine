0e3320e44664b18f3a02b080319ce81645c7aacd
Cameron Bothner <cbothner@umich.edu>
2013-06-08 21:55:01 -0400

    Initial commit with rails setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Guillotine::Application.config.secret_token = '0ee2b40c7c9e75e7f1161d276713f1a84c3a80fd5044553966e38dbef8fe955ce64bbcce545d6f497673701f1897511f2135f6c89db4107cc9cf51fa4b18a99c'
