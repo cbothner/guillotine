cca968e3eed92ee7b94c92ac2278a4ff4e6932d0
Cameron Bothner <cbothner@umich.edu>
2013-06-10 14:54:01 -0400

    Created a home view with flexbox sections; standard  table styles; better looking jQueryUI

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Guillotine::Application.routes.draw do
+  get "home/index"
+
   resources :rewards
 
 
@@ -67,7 +69,7 @@ Guillotine::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+   root :to => 'home#index'
 
   # See how all your routes lay out with "rake routes"
 
