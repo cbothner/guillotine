5cdf240b54fa66aa09e0825f846d91a1b777a312
キャメロンボスナー <cbothner@umich.edu>
2014-08-07 16:53:50 -0400

    Fixed it again. This is why you don't commit w/o testing

--- app/cells/donation_list_cell.rb
+++ app/cells/donation_list_cell.rb
@@ -5,10 +5,10 @@ class DonationListCell < Cell::Rails
 
   def show(params)
     @pledger = params[:pledger]
-    donations = pledger.donations.includes(slot: [:show, :semester])
+    donations = @pledger.donations.includes(slot: [:show, :semester])
     @activeDonations = donations.select { |d| d.active?(current_user == User.where(username: 'dd')[0]) }
     @archivedDonations = donations - @activeDonations
-    forgivenDonations = pledger.forgiven_donations.includes(slot: [:semester])
+    forgivenDonations = @pledger.forgiven_donations.includes(slot: [:semester])
     @forgivenTotal = forgivenDonations.reduce(0) { |sum, don| sum + don.amount }
     @forgivenSemesters = forgivenDonations.group_by { |d| d.slot.semester }.count
 
