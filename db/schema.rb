e89324a263f4f7e233e23794db52519da55d166e
Cameron Bothner <cbothner@umich.edu>
2014-12-21 00:36:18 -0500

    Added comments field to donations
    Currently only visible by the development director because its intended purpose
    is documenting recurring gifts, etc., and phone operators will definitely
    misinterpret it.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140605063406) do
+ActiveRecord::Schema.define(version: 20141221050557) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 20140605063406) do
     t.string   "phone_operator"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "comment"
   end
 
   add_index "donations", ["pledger_id"], name: "index_donations_on_pledger_id", using: :btree
