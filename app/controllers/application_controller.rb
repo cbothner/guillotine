9eb182da92debe11794cd84769267cce0357f081
Cameron Bothner <cbothner@umich.edu>
2016-12-06 16:41:55 -0500

    Log latex errors to stdout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,25 @@
 class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
+  around_filter :print_latex_errors
 
   protect_from_forgery
+
+  def print_latex_errors
+    begin
+      yield
+    rescue ActionView::Template::Error => e
+      raise unless e.message[/latex/]
+
+      logger.error "Failed to create PDF..."
+
+      log_file = e.message.scan(/\/.*\.log/).first
+      if log_file && File.exists?(log_file)
+        puts "--- Latex Log ---\n"
+        puts File.read(log_file)
+        puts "---    End    ---\n\n"
+      end
+    end
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :email
