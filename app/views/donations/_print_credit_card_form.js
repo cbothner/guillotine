0465edbc1e3ff8f27280542dd098688e4f9b88e5
Cameron Bothner <cameronbothner@gmail.com>
2017-06-18 11:26:04 -0400

    Factors out ajax printing code from donations/create.js and update.js

---
+++ app/views/donations/_print_credit_card_form.js
@@ -0,0 +1,23 @@
+function closePrint() {
+  document.body.removeChild(this.__container__);
+}
+
+function setPrint() {
+  this.contentWindow.__container__ = this;
+  this.contentWindow.onbeforeunload = closePrint;
+  this.contentWindow.onafterprint = closePrint;
+  this.contentWindow.focus(); // Required for IE
+  this.contentWindow.print();
+}
+
+var x = document.createElement('iframe');
+x.style.visibility = 'hidden';
+x.style.position = 'fixed';
+x.style.right = '0';
+x.style.bottom = '0';
+x.style.width = '8.5in';
+x.style.height = '11in';
+x.src = '/gpo/single/<%= donation_id %>';
+x.onload = setPrint;
+
+document.body.appendChild(x);
