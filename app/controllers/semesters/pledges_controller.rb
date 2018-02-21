b6358a6fe104d4bf31e6b1452e1b649d40c44199
Cameron Bothner <cameronbothner@gmail.com>
2018-02-20 21:01:18 -0500

    Adds a button to process pledges after the CSV is generated

--- app/controllers/semesters/pledges_controller.rb
+++ app/controllers/semesters/pledges_controller.rb
@@ -1,15 +1,23 @@
 module Semesters
   class PledgesController < ApplicationController
     before_action :authenticate_user!
+    before_action :set_semester
 
     layout 'generate'
 
     def show
-      set_semester
       set_donation_totals_by_pledger_id
       set_last_donation_time_by_pledger_id
       set_taxable_totals_by_pledger_id
       set_pledgers
+    end
+
+    def update
+      update_scoped_donations pledge_form_sent: true
+      update_scoped_rewards taxed: true
+
+      redirect_to semester_pledges_path(@semester),
+                  notice: 'Pledge forms marked sent; Rewards marked taxed'
     end
 
     private
@@ -20,20 +28,17 @@ module Semesters
 
     def set_donation_totals_by_pledger_id
       @donation_totals_by_pledger_id =
-        @semester.donations.merge(donation_scope)
-                 .group(:pledger_id).sum(:amount)
+        donation_scope.group(:pledger_id).sum(:amount)
     end
 
     def set_last_donation_time_by_pledger_id
       @last_donation_time_by_pledger_id =
-        @semester.donations.merge(donation_scope)
-                 .group(:pledger_id).maximum(:created_at)
+        donation_scope.group(:pledger_id).maximum(:created_at)
     end
 
     def set_taxable_totals_by_pledger_id
       @taxable_totals_by_pledger_id =
-        Reward.untaxed.joins(:item)
-              .group(:pledger_id).sum('items.taxable_value')
+        reward_scope.joins(:item).group(:pledger_id).sum('items.taxable_value')
       @taxable_totals_by_pledger_id.default = '0.0'
     end
 
@@ -42,8 +47,20 @@ module Semesters
                          .order(Arel.sql('lower(name) ASC'))
     end
 
+    def update_scoped_donations(params)
+      donation_scope.update_all params
+    end
+
+    def update_scoped_rewards(params)
+      reward_scope.update_all params
+    end
+
     def donation_scope
-      Donation.pledge_form_unsent.by_check
+      @semester.donations.pledge_form_unsent.by_check
+    end
+
+    def reward_scope
+      Reward.untaxed
     end
   end
 end
